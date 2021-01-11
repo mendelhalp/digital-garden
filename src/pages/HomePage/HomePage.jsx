a2bedef951566623dc299c62016a2b51070e56bf
Menachem Halperin <mendelhalp@gmail.com>
2021-01-12 01:40:34 +0200

    make the home page responsive

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,6 @@
 import './HomePage.css'
 import TopNavbar from '../../components/TopNavbar/TopNavbar';
+import logo from '../../images/logo192.png'
 
 const HomePage = () => {
 
@@ -7,7 +8,19 @@ const HomePage = () => {
     return (
         <div className="p-home">
             <TopNavbar activeLink='Home'/>
-        home page
+                <div className='main-content'>
+                    <div className='logo'>
+                        <img className='logo-img' src={logo} alt="logo"/>
+                    </div>
+                    <div className='home-divider'></div>
+                    <div className='main-text'>
+                        <div className='text'>
+                            <h2>הגן הדיגיטלי</h2>
+                            <h3>הקשר שלך עם ההורים</h3>
+                            <h3>נראה אחרת!</h3>
+                        </div>
+                    </div>
+                </div>
         </div>
     )
 }
