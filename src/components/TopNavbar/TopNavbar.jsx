d894ed65ad7d788b4721d2b7e362c32c39c5e7ad
Menachem Halperin <mendelhalp@gmail.com>
2021-01-11 13:58:06 +0200

    lighten the top navbar

--- src/components/TopNavbar/TopNavbar.jsx
+++ src/components/TopNavbar/TopNavbar.jsx
@@ -1,11 +1,15 @@
 import { Nav, Navbar, NavDropdown } from 'react-bootstrap';
 import './TopNavbar.css';
+import logo from '../../images/logo192.png';
 
 const TopNavbar = ({ activeLink }) => {
 
   return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-      <Navbar.Brand href="#/">הגן הדיגיטלי</Navbar.Brand>
+    <Navbar collapseOnSelect expand="lg" bg="light">
+      <Navbar.Brand href="#/" className='ml-2'>
+        <img src={logo} alt="Digital Garden logo" width='60' height='60' className="d-inline-block align-top"/>
+        {/* הגן הדיגיטלי */}
+      </Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="ml-auto">
