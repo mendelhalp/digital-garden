465d9b63a25df0c4207e9a4debd5d51ab889b10d
Menachem Halperin <mendelhalp@gmail.com>
2021-01-13 18:08:26 +0200

    get the galleries of the gan the user belongs to

--- src/pages/GalleriesPage/GalleriesPage.jsx
+++ src/pages/GalleriesPage/GalleriesPage.jsx
@@ -1,12 +1,35 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { Redirect } from 'react-router-dom';
 import TopNavbar from '../../components/TopNavbar/TopNavbar'
 import ActiveUserContext from '../../utils/ActiveUserContext';
+import Parse from 'parse';
 import './GalleriesPage.css'
 
 const GalleriesPage = (props) => {
     const {onLogout} = props;
+    const [galleries, setGalleries] = useState([]);
     const activeUser = useContext(ActiveUserContext);
+
+    useEffect(() => {
+        async function getGalleries () {
+            const parseUser = await new Parse.Query(new Parse.User()).get(activeUser.id);
+            const parseGan = await new Parse.Query(new Parse.Object.extend('Gan')).get(parseUser.get('gan').id);
+
+            const Gallery = Parse.Object.extend('Gallery');
+            const query = new Parse.Query(Gallery);
+            query.equalTo("gan", parseGan);
+            const results = await query.find();
+            setGalleries(results.map(gallery => 
+                    gallery.get('title')
+                ));
+        }
+
+        getGalleries();
+    },[]);
+
+
+    const galleriesView = [];
+
 
     if (!activeUser) {
         return <Redirect to="/"/>
@@ -15,7 +38,7 @@ const GalleriesPage = (props) => {
     return (
         <div className="p-galleries">
             <TopNavbar activeLink='Galleries' onLogout={onLogout}/>
-            galleries page
+            
         </div>
     )
 }
