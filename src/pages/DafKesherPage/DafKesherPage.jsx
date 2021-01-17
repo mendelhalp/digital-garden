a07b679e4ab3cd9e249e96522ed15774c92a4366
Menachem Halperin <mendelhalp@gmail.com>
2021-01-17 15:11:46 +0200

    create DafKesherPage

---
+++ src/pages/DafKesherPage/DafKesherPage.jsx
@@ -0,0 +1,15 @@
+import './DafKesherPage.css'
+
+import React from 'react';
+
+function DafKesherPage(props) {
+
+
+    return (
+        <div className='p-daf-kesher'>
+            
+        </div>
+    );
+}
+
+export default DafKesherPage;
