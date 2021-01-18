b1bd24032d7cf607ad9c48c7be1e88333e46fc75
Menachem Halperin <mendelhalp@gmail.com>
2021-01-18 16:11:28 +0200

    move the dapeyKesher request to separate function

--- src/pages/DapeyKesherPage/DapeyKesherPage.jsx
+++ src/pages/DapeyKesherPage/DapeyKesherPage.jsx
@@ -5,7 +5,7 @@ import DafKesherCard from '../../components/DafKesherCard/DafKesherCard';
 import Parse from 'parse';
 import ActiveUserContext from '../../utils/ActiveUserContext';
 import './DapeyKesherPage.css';
-import getHebrewDate from '../../utils/getHebrewDate';
+import getGardenDapeyKesher from '../../utils/getGardenDapeyKesher';
 
 const DapeyKesherPage = () => {
     const [dapeyKesher, setDapeyKesher] = useState([]);
@@ -14,22 +14,10 @@ const DapeyKesherPage = () => {
     useEffect(() => {
         async function getDapeyKesher() {
             const parseUser = await new Parse.Query(new Parse.User()).get(activeUser.id);
-            const parseGan = await new Parse.Query(new Parse.Object.extend('Gan')).get(parseUser.get('gan').id);
-
-            const query = new Parse.Query(Parse.Object.extend('DafKesher'));
-            query.equalTo('gan', parseGan);
-            query.descending("date");
-            const results = await query.find();
-
-            const dapeyKesher = results.map(dafKesher => {
-                return({
-                    'id':dafKesher.id,
-                    'title':dafKesher.get('title'),
-                    'hebDate':getHebrewDate(dafKesher.get('date'))
-                })
-            });
+            const dapeyKesher = await getGardenDapeyKesher(parseUser.get('gan').id);
             setDapeyKesher(dapeyKesher);
         }
+        
         getDapeyKesher();
     },[])
 
---
+++ src/utils/getGardenDapeyKesher.jsx
@@ -0,0 +1,23 @@
+import Parse from 'parse';
+import getHebrewDate from './getHebrewDate';
+
+async function getGardenDapeyKesher(ganId) {
+
+    const Gan = await new Parse.Query(new Parse.Object.extend('Gan')).get(ganId);
+    const dapeyKesherQuery = new Parse.Query(Parse.Object.extend('DafKesher'));
+    dapeyKesherQuery.equalTo("gan", Gan);
+    dapeyKesherQuery.descending("date");
+
+    const results = await dapeyKesherQuery.find();
+
+    const dapeyKesher = results.map(dafKesher => {
+        return({
+            'id':dafKesher.id,
+            'title':dafKesher.get('title'),
+            'hebDate':getHebrewDate(dafKesher.get('date'))
+        })
+    });
+
+    return (dapeyKesher);
+}
+export default getGardenDapeyKesher;
