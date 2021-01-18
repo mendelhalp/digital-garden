61bc536179753030b4fe9702e4947b81e6d99af2
Menachem Halperin <mendelhalp@gmail.com>
2021-01-18 13:17:23 +0200

    build DafKesherPage structuer

--- src/pages/DafKesherPage/DafKesherPage.jsx
+++ src/pages/DafKesherPage/DafKesherPage.jsx
@@ -1,13 +1,35 @@
 import './DafKesherPage.css'
-
 import React from 'react';
+import { Card, Col, Container, Row } from 'react-bootstrap';
+import { useParams } from 'react-router-dom';
 
 function DafKesherPage(props) {
 
+    const dafKesherId = useParams().id;
+
+    const topicsView = 'topics';
+
+    const messagesView = 'messages';
 
     return (
         <div className='p-daf-kesher'>
-            
+            <Container>
+                <h2>{`דף קשר ${dafKesherId}`}</h2>
+                <Row>
+                    <Col md={8}>
+                        <Card>
+                            <Card.Header as='h5'>מה למדנו השבוע?</Card.Header>
+                            {topicsView}
+                        </Card>
+                    </Col>
+                    <Col md={4}>
+                        <Card>
+                            <Card.Header as='h5'>הודעות</Card.Header>
+                            {messagesView}
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }
