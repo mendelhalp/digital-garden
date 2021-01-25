4b9714c77b0ee1e459d9e553dad535a5c6110b8d
Menachem Halperin <mendelhalp@gmail.com>
2021-01-25 17:05:01 +0200

    switch arrows in ImageModal to icons

--- src/components/ImageModal/ImageModal.jsx
+++ src/components/ImageModal/ImageModal.jsx
@@ -1,3 +1,5 @@
+import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import { Button, Col, Image, Modal } from 'react-bootstrap';
 import './ImageModal.css'
@@ -25,14 +27,14 @@ const ImageModal = (props) => {
         <Modal size='md' show={showModal} onHide={close} centered className='c-image-modal'>
             <Modal.Body className='p-0 row'>
                 <Col xs={1} className='prev'>
-                    <a className='prev' variant='dark' onClick={prevImage}>&#10094;</a>
+                    <a className='prev' variant='dark' onClick={prevImage}><FontAwesomeIcon icon={faChevronRight}/></a>
                 </Col>
                 <Col xs={10} className='p-0'>
                     <Image src={images[selectedImage].url} />
                     <Button onClick={close} className='modal-x-button' variant='outline-warning'>X</Button>
                 </Col>
                 <Col xs={1} className='next'>
-                    <a className='next' variant='dark' onClick={nextImage}>&#10095;</a>
+                    <a className='next' variant='dark' onClick={nextImage}><FontAwesomeIcon icon={faChevronLeft}/></a>
                 </Col>
             </Modal.Body>
         </Modal>
