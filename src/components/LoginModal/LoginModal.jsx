f90e7b9a4e339946cfdb5bb2367770e9c37a9dc5
Menachem Halperin <mendelhalp@gmail.com>
2021-01-26 09:46:47 +0200

    adding Requesting Password Reset

--- src/components/LoginModal/LoginModal.jsx
+++ src/components/LoginModal/LoginModal.jsx
@@ -1,10 +1,9 @@
 import React, { useEffect } from 'react';
 import { useState } from 'react';
-import { Button, Modal, Form, Alert } from 'react-bootstrap';
+import { Button, Modal, Form, Alert, Card } from 'react-bootstrap';
 import Parse from 'parse';
 import UserModel from '../../model/UserModel';
 import isEnterPressed from '../../utils/IsEnterPressed';
-import { Link } from 'react-router-dom';
 import isEmailValid from '../../utils/isEmailValid';
 
 function LoginModal(props) {
@@ -13,11 +12,19 @@ function LoginModal(props) {
     const [pwd, setPwd] = useState('');
     const [showError, setShowError] = useState(false);
     const [showEmailError, setShowEmailError] = useState(false);
+    const [forgotPwdEmail, setForgotPwdEmail] = useState('');
+    const [showForgotPwdEmail, setShowForgotPwdEmail] = useState(false);
+    const [showForgotPwdEmailError, setShowForgotPwdEmailError] = useState(false);
+    const [showResetsecces, setShowResetsecces] = useState(false);
 
     function cleanFormFields () {
         setEmail('');
         setPwd('');
         setShowError(false);
+        setForgotPwdEmail('');
+        setShowForgotPwdEmail(false);
+        setShowForgotPwdEmailError(false);
+        setShowResetsecces(false);
     }
 
     useEffect(() => {
@@ -33,11 +40,17 @@ function LoginModal(props) {
             setShowEmailError(false);
         }
     },[email]);
+    useEffect(() => {
+        if (!isEmailValid(forgotPwdEmail) && forgotPwdEmail !== '') {     //showing email pattern error if the email is not valid
+            setShowForgotPwdEmailError(true);
+        } else if (isEmailValid(forgotPwdEmail)) { 
+            setShowForgotPwdEmailError(false);
+        }
+    },[forgotPwdEmail]);
 
     async function login () {
         try {
             const parseUser = await Parse.User.logIn(email, pwd);
-            
             // Trigger onLogin event and clean the fields
             onLogin(new UserModel(parseUser));
             handleCloseLogin();
@@ -49,21 +62,36 @@ function LoginModal(props) {
             setShowError(true);
         }
     }
-
+    
     function close () {
         handleCloseLogin();
         cleanFormFields();
     }
-
+    
     function ifEnterPressed (event) {                   //detecting if Enter pressed and executing login
         if (isEnterPressed(event) && email){
             login();
         }
     }
-
+    
     function showSignup(){
         close();
         showSignupModal();
+    }
+    
+    function handleForgotPassword(){
+        Parse.User.requestPasswordReset("email@example.com").then(() => {
+            // Password reset request was sent successfully
+            if (typeof document !== 'undefined') {
+                setShowResetsecces(true);
+                setShowForgotPwdEmail(false);
+                console.log('Reset password email sent successfully');
+            }
+        }).catch((error) => {
+            if (typeof document !== 'undefined') {
+                console.error('Error while creating request to reset user password', error);
+            }
+        })
     }
 
     return (
@@ -79,11 +107,22 @@ function LoginModal(props) {
                             onChange={e => { setEmail(e.target.value) }} onKeyPress={ifEnterPressed} />
                         <Form.Control.Feedback type="invalid">כתובת מייל לא תקינה</Form.Control.Feedback>
                     </Form.Group>
-                    <Form.Group controlId='loginPwd'>
+                    <Form.Group controlId='loginPwd' className='mb-0'>
                         <Form.Label>סיסמה</Form.Label>
-                        <Form.Control type="password" placeholder="סיסמה" value={pwd}
+                        <Form.Control type="password" value={pwd}
                             onChange={e => {setPwd(e.target.value)}}  onKeyPress={ifEnterPressed}/>
+                        <Button variant='link' onClick={() => {setShowForgotPwdEmail(true)}}>שכחתי סיסמה</Button>
                     </Form.Group>
+                        {showForgotPwdEmail && <Card.Body>
+                            <Form.Group controlId='forgotPwdEmail'>
+                                <Form.Label>דואר אלקטרוני לאיפוס סיסמה</Form.Label>
+                                <Form.Control type="email" value={forgotPwdEmail} className={showForgotPwdEmailError ? 'is-invalid' : null}
+                                    onChange={e => { setForgotPwdEmail(e.target.value) }} />
+                                <Form.Control.Feedback type="invalid">כתובת מייל לא תקינה</Form.Control.Feedback>
+                                <Button variant='dark' className='mt-2' onClick={handleForgotPassword}>אפס סיסמה</Button>
+                            </Form.Group>
+                        </Card.Body>}
+                        {showResetsecces && <Form.Label>נשלח מייל לאיפוס סיסמה</Form.Label>}
                 </Form>
                 {showError ? <Alert variant="danger">מייל או סיסמה שגויים</Alert> : null}
             </Modal.Body>
