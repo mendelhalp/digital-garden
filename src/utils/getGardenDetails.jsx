a9dfc8350756a9f3e86c7594fc34d4dd0bc63fd4
Menachem Halperin <mendelhalp@gmail.com>
2021-02-10 14:33:06 +0200

    reducing the ajax calls + get year in getGardenDetails

--- src/utils/getGardenDetails.jsx
+++ src/utils/getGardenDetails.jsx
@@ -1,16 +1,17 @@
 import Parse from 'parse';
 
 async function getGardenDetails(activeUser) {
-    const parseUser = await new Parse.Query(new Parse.User()).get(activeUser.id);
-    const parseGan = await new Parse.Query(new Parse.Object.extend('Gan')).get(parseUser.get('gan').id);
+    const parseGan = await new Parse.Query(new Parse.Object.extend('Gan')).get(activeUser.gardenId);
     const ganName = parseGan.get('name');
     const ganLogo = parseGan.get('logo')._url;
+    const ganYear = parseGan.get('year');
 
     const gardenDetails = {
         'parseGarden':parseGan,
         'id':parseGan.id,
         'name':ganName,
-        'logo':ganLogo
+        'logo':ganLogo,
+        'year':ganYear
     }
     return (gardenDetails);
 }
