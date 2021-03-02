307da660c4ee67f692416847176d3f8ce19ef046
Menachem Halperin <mendelhalp@gmail.com>
2021-03-03 01:32:05 +0200

    always showing edit buttons to manager also in dafKesher + gallery

--- src/components/ImageCard/ImageCard.jsx
+++ src/components/ImageCard/ImageCard.jsx
@@ -1,7 +1,7 @@
 import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
-import { Card } from 'react-bootstrap';
+import { Card, Col } from 'react-bootstrap';
 import './ImageCard.css';
 
 const ImageCard = (props) => {
@@ -12,7 +12,9 @@ const ImageCard = (props) => {
                 {image ? <Card.Img variant="top" src={image.url}  onClick={onClick} /> : null}
                 {activeUser && activeUser.role === 'manager' &&                                     //showing edit icons only to garden owner
                     <Card.Footer>
-                        <FontAwesomeIcon icon={faTrashAlt} onClick={() => {handleDeleteClick(image)}}/>
+                        <Col onClick={() => {handleDeleteClick(image)}}>
+                            <FontAwesomeIcon icon={faTrashAlt}/>
+                        </Col>
                     </Card.Footer>}
             </Card>
         </div>
